b972432c8199cf87d659d9a8945f6675f065587a
Daniel Miller <danielmillerdev@gmail.com>
2019-10-17 14:02:25 -0500

    Updated 16port scripts to Python 3 syntax.

--- 16port/script16_2.py
+++ 16port/script16_2.py
@@ -10,8 +10,8 @@ pinList = [2, 3, 4, 17, 27, 22, 10, 9, 11, 5, 6, 13, 19, 26, 21, 20]
 
 # loop through pins and set mode and state to 'low'
 
-for i in pinList: 
-    GPIO.setup(i, GPIO.OUT) 
+for i in pinList:
+    GPIO.setup(i, GPIO.OUT)
     GPIO.output(i, GPIO.HIGH)
 
 # time to sleep between operations in the main loop
@@ -22,7 +22,7 @@ SleepTimeL = 0.2
 
 try:
   while True:
-   
+
     for i in pinList:
         GPIO.output(i, GPIO.HIGH)
         time.sleep(SleepTimeL);
@@ -39,7 +39,7 @@ try:
 
 # End program cleanly with keyboard
 except KeyboardInterrupt:
-  print "  Quit"
+  print ("  Quit")
 
   # Reset GPIO settings
   GPIO.cleanup()
